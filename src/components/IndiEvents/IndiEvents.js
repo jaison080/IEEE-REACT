e5ffd024067297c8aa8779d89084fd072771f4e4
Bharath5324 <bharath.s@ieee.org>
2020-04-08 23:07:48 +0530

    Positioning back button in 404 page, and additions to events page

--- src/components/IndiEvents/IndiEvents.js
+++ src/components/IndiEvents/IndiEvents.js
@@ -1,61 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
-
-
+import './IndiEvents.css'
 function IndiEvents(props){
+
    return(
        <div className="outer">
-
-            {/*<a class="btn-floating btn-large waves-effect waves-light red" style={{"position":"fixed", "bottom": "0", "right":"0"}}><i class="material-icons">chevron_left</i></a>*/}
-
-  <nav>
-    <div class="nav-wrapper" stye={{"background-color": "white"}}>
-      <div class="col s12">
-        <a onClick={props.closeHandler} style={{"cursor":"pointer"}} class="breadcrumb">Events</a>
-        <a  class="breadcrumb">{props.activeitem.title}</a>
-        </div>
-    </div>
-  </nav>
-            
-           <div className="row">
-               <div className="col l12">
-               
-               <img class="responsive-img" style={{"width":"100%"}} src={require('../EventImages/'+ props.activeitem.img_src +'.jpg')}/>
-            
-               </div>
-           </div>
-           <div className="row">
-               <div className="col l12">
-               
-               <div style={{ "text-align": "center", "padding-bottom": "100px", "padding-right": "50px", "padding-left": "50px"}} className="heading">
-               <h1>{props.activeitem.title}</h1>
-                </div>
-            
-               </div>
-           </div>
-           <div className="row">
-               <div className="col m4 s12">
-               <p>{props.activeitem.date}</p>
-                </div>
-                <div className="col m4 s12">
-               <p>{props.activeitem.location}</p>
-                </div>
-                <div className="col m4 s12">
-               <p>{props.activeitem.type}</p>
-                </div>
-           </div>
-           <div className="row">
-               <div className="col s12 m12 l12">
-               
-               <div style={{ "text-align": "center", "padding-bottom": "100px", "padding-right": "50px", "padding-left": "50px"}} className="heading">
-               <p>{props.activeitem.description}</p>
+            <div class="light-blue-text text-darken-4 white">
+                <h1 style={{"line-height":"74px"}}>Event Details</h1>
             </div>
-            
+            <nav>
+                <div class="light-blue darken-4 nav-wrapper">
+                    <div class="col s12">
+                         <a onClick={props.closeHandler} style={{"cursor":"pointer"}} class="breadcrumb">Events</a>
+                         <a  class="breadcrumb">{props.activeitem.title}</a>
+                    </div>
+                </div>
+            </nav>
+
+           <div className="top row">
+               <div className="col s12 m12 l8">
+                    <img class="responsive-img" style={{"width":"100%"}} src={require('../EventImages/'+ props.activeitem.img_src +'.jpg')}/>
                </div>
+               <div className="inform col s12 m12 l4">
+                    <p className="head"><i className="material-icons left">explore</i> {props.activeitem.location}</p>
+                    <p className="head"><i className="material-icons left">event</i> {props.activeitem.date}</p>
+                    <p className="head"><i className="material-icons left">keyboard_arrow_right</i> {props.activeitem.type}</p>
+                </div>
+           </div>
+           <div>
+
+                    <div style={{ "text-align": "center", "transition": "all 0.3s linear", "padding-right": "50px", "padding-left": "50px"}} className="heading">
+                        <h1 className="head">{props.activeitem.title}</h1>
+                    </div>
+
            </div>
 
-       </div>
-   )
+           <div className="container">
+
+                <div style={{ "text-align": "justify", "padding-bottom": "100px", "padding-right": "50px", "padding-left": "50px", "color": "#777777"}} className="heading">
+                    <p>{props.activeitem.description}</p>
+                </div>
+           </div>
+
+        </div>
+    )
 
 
     }
--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -8,10 +8,10 @@ const NotFoundPage= () => {
       useEffect(() => {
       let parallel = document.querySelector('.parallax');
       M.Parallax.init(parallel, {});
-  
+
     }, [])
     return (
-      <div style={{"display": "flex"}} className="parallax-container hello">
+      <div className="parallax-container hello center-align">
         <div className="parallax"><img className="responsive-img" style={{"min-height": "0.01%"}} src={require("./404.png")} /></div>
         <div style={{ "position": "sticky", "top" : "79%" }} id="nfb">
         <a className="waves-effect waves-light light-blue darken-4 btn notfoundbutton" id="g"><i className="material-icons left">arrow_back</i><Link to="/" style={{'color':'white'}} >Back</Link></a>
