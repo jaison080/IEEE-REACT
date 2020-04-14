50216816e4e4c8a4b957adc468ddb44cef512985
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-04-14 16:17:54 +0000

    changes general

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -13,7 +13,7 @@ const Landing = props =>{
       <div className="intro"><Intro/></div>
       <div className="mission"><Missionvision/></div>
       <div className="about"><About/></div>
-      <div className="gallery"><Photos/></div>
+      
     </div>
     </body>
   );
