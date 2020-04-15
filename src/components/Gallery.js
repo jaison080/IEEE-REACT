36313e2ae466af49dbc23babf74dc9aabeaac320
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-04-15 12:56:45 +0000

    changed timeline and gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -15,43 +15,44 @@ const Gallery = () => {
 			<div className="container center">
 				<h3 className='light-blue-text text-darken-3 center-align' id="bottom_margin">Gallery</h3>
 				<div class="row">
-
+			
 					<div class="col s12 m4" id="bottom_margin">
-					<Fade left>
+					
 						<img style={{ 'backgroundSize':'cover' , 'height':'35vh', 'margin':'auto'}} src={require("../assets/1.jpg")} alt="" className="materialboxed responsive-img mobile_space"  id="curve_border"/>
-					</Fade>
+					
 					</div>
-
-
+				
+				
 					<div class="col s12 m4" id="bottom_margin">
-					<Fade top>
-						<img style={{  'backgroundSize':'cover' , 'height':'35vh', 'margin':'auto'}} src={require("../assets/8.jpg")} alt="" class="materialboxed responsive-img mobile_space"  id="curve_border"/>
-					</Fade>
-					</div>
 
+						<img style={{  'backgroundSize':'cover' , 'height':'35vh', 'margin':'auto'}} src={require("../assets/8.jpg")} alt="" class="materialboxed responsive-img mobile_space"  id="curve_border"/>
+					
+					</div>
+				
 
 					<div class="col s12 m4">
-					<Fade right>
+					
 						<img style={{ 'backgroundSize':'cover' , 'height':'35vh', 'margin':'auto'}} src={require("../assets/7.jpg")} alt="" class="materialboxed responsive-img mobile_space"  id="curve_border"/>
-					</Fade>
+					
 					</div>
-
+				
 				</div>
 				<div class="row">
+			
 					<div class="col s12 m4" id="bottom_margin">
-					<Fade left>
+					
 						<img style={{ 'backgroundSize':'cover' , 'height':'35vh', 'margin':'auto' }} src={require("../assets/6.jpg")} alt="" class="materialboxed responsive-img hidepic"  id="curve_border"/>
-					</Fade>
+					
 					</div>
+				
 					<div class="col s12 m4" id="bottom_margin">
-					<Fade bottom>
+				
 						<img style={{ 'backgroundSize':'cover' , 'height':'35vh', 'margin':'auto'}} src={require("../assets/5.jpg")} alt="" class="materialboxed responsive-img mobile_space hidepic"  id="curve_border"/>
-          </Fade>
+        
 					</div>
 					<div class="col s12 m4" id="bottom_margin">
-					<Fade right>
 						<img style={{ 'backgroundSize':'cover' , 'height':'35vh', 'margin':'auto'}} src={require("../assets/2.jpg")} alt="" class="materialboxed responsive-img mobile_space hidepic"  id="curve_border"/>
-          </Fade>
+        
 					</div>
 				</div>
 
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -32,8 +32,8 @@ const toolbar= props =>(
                     <ul>
                         <li><NavLink to="/" exact activeStyle={{  textDecoration: "underline", textDecorationThickness: "2px"}}>Home   </NavLink></li>
                         <li><NavLink to="/execom" activeStyle={{  textDecoration: "underline", textDecorationThickness: "2px"}}>Execom</NavLink></li>
-                        <li><NavLink to="/achievements" activeStyle={{  textDecoration: "underline", textDecorationThickness: "2px"}}>Achievements</NavLink></li>
                         <li><NavLink to="/timeline" activeStyle={{  textDecoration: "underline", textDecorationThickness: "2px"}}>Timeline</NavLink></li>
+                        <li><NavLink to="/gallery" activeStyle={{  textDecoration: "underline", textDecorationThickness: "2px"}}>Gallery</NavLink></li>
                         <li><NavLink to="/events" activeStyle={{  textDecoration: "underline", textDecorationThickness: "2px"}}>Events</NavLink></li>
                     </ul>
                 </div>
