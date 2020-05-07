a3eb2243247ab4734a2b550e10c2b3458c3d620d
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-05-07 20:16:05 +0530

    before pull

---
+++ src/components/Title/Title.js
@@ -0,0 +1,21 @@
+import React from  'react';
+import { useEffect } from 'react';
+import './Title.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import image from '../../neon.jpg';
+
+const Parallax = () => {
+    useEffect(() => {
+		let parallel = document.querySelector('.parallax');
+		M.Parallax.init(parallel, {});
+
+	}, [])
+    return(
+            <div class="parallax-container">
+                <div class="parallax"><img src={image} /></div>
+            </div>
+    )
+}
+
+export default Parallax;
