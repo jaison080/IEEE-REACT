6dca4d8ff35a1e60286f60831ffb97aed942b0a6
Bharath5324 <bharath.s@ieee.org>
2021-07-31 19:40:46 +0530

    Updates

---
+++ precache-manifest.efc6e4ad009827adc6635caefb825d83.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c8c16d5984a2f9ceceffd29762725a",
+    "url": "/IEEE-REACT/index.html"
+  },
+  {
+    "revision": "b78d063aef6749e9d39e",
+    "url": "/IEEE-REACT/static/css/2.eefbb0aa.chunk.css"
+  },
+  {
+    "revision": "acc98d90ca5bd5c32f4e",
+    "url": "/IEEE-REACT/static/css/main.9761e431.chunk.css"
+  },
+  {
+    "revision": "b78d063aef6749e9d39e",
+    "url": "/IEEE-REACT/static/js/2.6d9ae4f0.chunk.js"
+  },
+  {
+    "revision": "d8b4173f7a8b6c66c369ccd5944b6a3e",
+    "url": "/IEEE-REACT/static/js/2.6d9ae4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acc98d90ca5bd5c32f4e",
+    "url": "/IEEE-REACT/static/js/main.2966b180.chunk.js"
+  },
+  {
+    "revision": "9fa171d943c0f2665183",
+    "url": "/IEEE-REACT/static/js/runtime-main.1cf62cec.js"
+  },
+  {
+    "revision": "56479331460f619d3d43b5d87e6a41ce",
+    "url": "/IEEE-REACT/static/media/1.56479331.jpg"
+  },
+  {
+    "revision": "2ee4c113e5b5b8ef4adfed542b4a168d",
+    "url": "/IEEE-REACT/static/media/3.2ee4c113.jpg"
+  },
+  {
+    "revision": "4aceb0ef52473718662dd64db1afc21d",
+    "url": "/IEEE-REACT/static/media/4.4aceb0ef.jpg"
+  },
+  {
+    "revision": "a81f369f788126588489e06b8dac21a7",
+    "url": "/IEEE-REACT/static/media/404.a81f369f.png"
+  },
+  {
+    "revision": "4073702a0e3b7773f0ef64f733ffef59",
+    "url": "/IEEE-REACT/static/media/5.4073702a.jpg"
+  },
+  {
+    "revision": "578dd2df3a230fb8eb316351ba3a3e50",
+    "url": "/IEEE-REACT/static/media/6.578dd2df.jpg"
+  },
+  {
+    "revision": "48fae713872bbb3d1b4b9100b0f12402",
+    "url": "/IEEE-REACT/static/media/7.48fae713.jpg"
+  },
+  {
+    "revision": "e6fa1f4c296c70064ee0019fd5372fff",
+    "url": "/IEEE-REACT/static/media/8.e6fa1f4c.jpg"
+  },
+  {
+    "revision": "d14bcb7bc54138ee6c15e8ce438825ef",
+    "url": "/IEEE-REACT/static/media/AC.d14bcb7b.jpg"
+  },
+  {
+    "revision": "d196213f50557194321ed425c445205a",
+    "url": "/IEEE-REACT/static/media/CS.d196213f.jpeg"
+  },
+  {
+    "revision": "f0530f589c7a9600f50063bca7303dbc",
+    "url": "/IEEE-REACT/static/media/CS_chair.f0530f58.jpg"
+  },
+  {
+    "revision": "b584a1993e6fe9fab725a5c7f8330649",
+    "url": "/IEEE-REACT/static/media/CS_secretary.b584a199.jpg"
+  },
+  {
+    "revision": "790f6a0a3a636948f814f9c501fb34b8",
+    "url": "/IEEE-REACT/static/media/Christina.790f6a0a.jpg"
+  },
+  {
+    "revision": "635027cbbd865d7f1efb7b753fc5f6db",
+    "url": "/IEEE-REACT/static/media/Content_head.635027cb.jpg"
+  },
+  {
+    "revision": "be2efd136cc12e480e6b4e0f83d17e9c",
+    "url": "/IEEE-REACT/static/media/IAS.be2efd13.jpeg"
+  },
+  {
+    "revision": "73099c7dbc7fea1ca482c9c54f9b992c",
+    "url": "/IEEE-REACT/static/media/IAS_secretary.73099c7d.jpg"
+  },
+  {
+    "revision": "1c592d5fe24eb133e9f6e6a6e8b37099",
+    "url": "/IEEE-REACT/static/media/Link_rep.1c592d5f.jpg"
+  },
+  {
+    "revision": "be388d959fa871138cf07bc9327bf6c4",
+    "url": "/IEEE-REACT/static/media/MDC.be388d95.jpg"
+  },
+  {
+    "revision": "b129da7c9df24e5586e120a7d964b38a",
+    "url": "/IEEE-REACT/static/media/PES.b129da7c.jpeg"
+  },
+  {
+    "revision": "2d254a1620eaf7b7fcb3a18381863d10",
+    "url": "/IEEE-REACT/static/media/PES_chair.2d254a16.jpg"
+  },
+  {
+    "revision": "b88c0fd8b54f39cba55890a0c28595e8",
+    "url": "/IEEE-REACT/static/media/PES_secretary.b88c0fd8.jpg"
+  },
+  {
+    "revision": "cccd9cfa0f909867ed0d3c883f8921ca",
+    "url": "/IEEE-REACT/static/media/SB_logo.cccd9cfa.png"
+  },
+  {
+    "revision": "93f50b73aa6f35f67df4cab2639ffc56",
+    "url": "/IEEE-REACT/static/media/SB_logo_white.93f50b73.png"
+  },
+  {
+    "revision": "3039490ab7d5dab5f57e1e00b0f2a5f1",
+    "url": "/IEEE-REACT/static/media/SPS.3039490a.jpeg"
+  },
+  {
+    "revision": "38ecff2c3870cc13e40593f55d43bdd0",
+    "url": "/IEEE-REACT/static/media/SPS_chair.38ecff2c.jpg"
+  },
+  {
+    "revision": "d7df027b305cb4d785ed109c89002854",
+    "url": "/IEEE-REACT/static/media/SPS_secretary.d7df027b.jpg"
+  },
+  {
+    "revision": "3da4e0cfcbbda413b01966f2444ebca1",
+    "url": "/IEEE-REACT/static/media/Secretary.3da4e0cf.jpg"
+  },
+  {
+    "revision": "6ae1a0e9b7eb885acae58088703de326",
+    "url": "/IEEE-REACT/static/media/TC.6ae1a0e9.jpg"
+  },
+  {
+    "revision": "9d63b3c2e9e5c9e2d9b8c8346d62c14e",
+    "url": "/IEEE-REACT/static/media/VC.9d63b3c2.jpg"
+  },
+  {
+    "revision": "68f11796c34db62c5899b94c77ece491",
+    "url": "/IEEE-REACT/static/media/WiE.68f11796.jpeg"
+  },
+  {
+    "revision": "1124abc68b49c398a78907028ba1dc72",
+    "url": "/IEEE-REACT/static/media/WiE_chair.1124abc6.jpg"
+  },
+  {
+    "revision": "4e4db00f8bfd4c1c561be40ddab3e66d",
+    "url": "/IEEE-REACT/static/media/WiE_secretary.4e4db00f.jpg"
+  },
+  {
+    "revision": "0a4b606d0d890a317b99d4e9afe5f957",
+    "url": "/IEEE-REACT/static/media/gokul.0a4b606d.jpg"
+  },
+  {
+    "revision": "ab28facd6d246639270d8356df86ab2f",
+    "url": "/IEEE-REACT/static/media/hero.ab28facd.gif"
+  },
+  {
+    "revision": "9c3b21a8cb4fd53fd78ef6ded92cbd09",
+    "url": "/IEEE-REACT/static/media/intramun.9c3b21a8.jpg"
+  },
+  {
+    "revision": "49b9915fbc84177b570f3294bea3f90e",
+    "url": "/IEEE-REACT/static/media/linux.49b9915f.jpg"
+  },
+  {
+    "revision": "20224a578338ef1309bb1b8eaede9612",
+    "url": "/IEEE-REACT/static/media/magic(2).20224a57.jpg"
+  },
+  {
+    "revision": "f03fc6726d01d4d7d7a7b96292f64dbe",
+    "url": "/IEEE-REACT/static/media/magic(3).f03fc672.jpg"
+  },
+  {
+    "revision": "fe969d8abe6b241d07426560a321176b",
+    "url": "/IEEE-REACT/static/media/magic.fe969d8a.jpg"
+  },
+  {
+    "revision": "8254261e64e9e28b6b3d74743a3519d9",
+    "url": "/IEEE-REACT/static/media/main1.8254261e.jpg"
+  },
+  {
+    "revision": "80b563dc1165b8a71b2f5fcb6baa3ee7",
+    "url": "/IEEE-REACT/static/media/matlab.80b563dc.jpg"
+  },
+  {
+    "revision": "e32ea3ad3d021ae62c1d4eb380f2b0d4",
+    "url": "/IEEE-REACT/static/media/mec.e32ea3ad.jpeg"
+  },
+  {
+    "revision": "0d8c1fa665739ba3b898e9ff5342cbcc",
+    "url": "/IEEE-REACT/static/media/pp.0d8c1fa6.jpg"
+  }
+]);
