dffa81d80d119689ad05418e1469562ee481e1d9
Madhav5046 <madhav5046@gmail.com>
2021-11-03 19:13:03 +0530

    Testimonial changed

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -96,32 +96,277 @@ class Testimonials extends Component{
           indicators: true
       });
       }
-  render(){
-    return (
-        <div ref={Carousel => {this.Carousel = Carousel;}} className="container carousel carousel-slider center">
-          <div class="carousel-fixed-item center">
+      render(){
+        return (
+            <div ref={Carousel => {this.Carousel = Carousel;}} className="container carousel carousel-slider center">
+              <div class="carousel-fixed-item center">
+              </div>
+              <div class="carousel-item white" href="#one!">
+              <div class="row">
+    <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
           </div>
-          <div class="carousel-item red" href="#one!">
-            <h2>First Panel</h2>
-            <p class="white-text">This is your first panel</p>
           </div>
-          <div class="carousel-item amber white-text" href="#two!">
-            <h2>Second Panel</h2>
-            <p class="white-text">This is your second panel</p>
-          </div>
-          <div class="carousel-item green white-text" href="#three!">
-            <h2>Third Panel</h2>
-            <p class="white-text">This is your third panel</p>
-          </div>
-          <div class="carousel-item blue white-text" href="#four!">
-            <h2>Fourth Panel</h2>
-            <p class="white-text">This is your fourth panel</p>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
           </div>
         </div>
-
-  )
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
+          </div>
+          </div>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
+          </div>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
+          </div>
+          </div>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
+          </div>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    
+  </div>
+  
+              </div>
+              <div class="carousel-item white" href="#two!">
+              <div class="row">
+              <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
+          </div>
+          </div>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
+          </div>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
+          </div>
+          </div>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
+          </div>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
+          </div>
+          </div>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
+          </div>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    
+  </div>
+              </div>
+              <div class="carousel-item white" href="#three!">
+              <div class="row">
+              <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
+          </div>
+          </div>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
+          </div>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
+          </div>
+          </div>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
+          </div>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
+          </div>
+          </div>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
+          </div>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    
+  </div>
+              </div>
+              <div class="carousel-item white " href="#four!">
+              <div class="row">
+              <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
+          </div>
+          </div>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
+          </div>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
+          </div>
+          </div>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
+          </div>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    <div class="col s4 m4 l4 " >
+      <div class="card small waves-effect hoverable">
+        <div class= 'row valign-wrapper'>
+          <div class='col s4'>
+        <div class="card-image">
+          <img  height='150' width="150" src={require("./Images/CS.jpeg")} class="circle" alt="" />
+          </div>
+          </div>
+          <div class='col s8'>
+            <span class="center-align">The name shall go here, hope this works
+            </span>
+          </div>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+      </div>
+    </div>
+    
+  </div>
+              </div>
+            </div>
+    
+      )
+        }
     }
-}
-
-
-export default Testimonials;    
+    
+    
+    export default Testimonials;    
