7a0fb98537aeb1fc4d6dce6c6776400701cd497d
Bharath Srinivasan <bharath.s@ieee.org>
2021-12-29 20:03:09 +0530

    Updates

---
+++ precache-manifest.3f67a006aaffef8d975f898ebe43b894.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475830919af5599e7efb353156f30d29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d69f58ac3dd6efa61cc4",
+    "url": "/static/css/2.e55c63d3.chunk.css"
+  },
+  {
+    "revision": "8406dc479d4500081529",
+    "url": "/static/css/main.16231036.chunk.css"
+  },
+  {
+    "revision": "d69f58ac3dd6efa61cc4",
+    "url": "/static/js/2.a478367f.chunk.js"
+  },
+  {
+    "revision": "d8b4173f7a8b6c66c369ccd5944b6a3e",
+    "url": "/static/js/2.a478367f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8406dc479d4500081529",
+    "url": "/static/js/main.bd30941d.chunk.js"
+  },
+  {
+    "revision": "a8229df926a3b5869ee3",
+    "url": "/static/js/runtime-main.778aca28.js"
+  },
+  {
+    "revision": "a81f369f788126588489e06b8dac21a7",
+    "url": "/static/media/404.a81f369f.png"
+  },
+  {
+    "revision": "d196213f50557194321ed425c445205a",
+    "url": "/static/media/CS.d196213f.jpeg"
+  },
+  {
+    "revision": "be2efd136cc12e480e6b4e0f83d17e9c",
+    "url": "/static/media/IAS.be2efd13.jpeg"
+  },
+  {
+    "revision": "3ffc349918f101c466fe0b8c2eff78e4",
+    "url": "/static/media/IEEE-logo-test.3ffc3499.svg"
+  },
+  {
+    "revision": "b129da7c9df24e5586e120a7d964b38a",
+    "url": "/static/media/PES.b129da7c.jpeg"
+  },
+  {
+    "revision": "93f50b73aa6f35f67df4cab2639ffc56",
+    "url": "/static/media/SB_logo_white.93f50b73.png"
+  },
+  {
+    "revision": "c2607e6a470c4d4f784eda795ccae8ed",
+    "url": "/static/media/SIGHT.c2607e6a.jpeg"
+  },
+  {
+    "revision": "3039490ab7d5dab5f57e1e00b0f2a5f1",
+    "url": "/static/media/SPS.3039490a.jpeg"
+  },
+  {
+    "revision": "68f11796c34db62c5899b94c77ece491",
+    "url": "/static/media/WiE.68f11796.jpeg"
+  },
+  {
+    "revision": "d9821eec8f11df57f28d636389ba8306",
+    "url": "/static/media/embs.d9821eec.png"
+  },
+  {
+    "revision": "e4ea719a3af69da04b485e37665ac051",
+    "url": "/static/media/juliana.e4ea719a.jpg"
+  },
+  {
+    "revision": "74fe876e18efbf3928e13fd4b5cd542b",
+    "url": "/static/media/mec.74fe876e.jpeg"
+  },
+  {
+    "revision": "be4cc2a9a4c10b78d7ca49483a6c7406",
+    "url": "/static/media/sahil.be4cc2a9.jpeg"
+  },
+  {
+    "revision": "c845b7a5ac3414617d9edc053642e35d",
+    "url": "/static/media/sreehari.c845b7a5.jpg"
+  },
+  {
+    "revision": "8d2e9542675e5e89ec9d2aee3f5d5a87",
+    "url": "/static/media/trial.8d2e9542.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/IEEE-REACT/precache-manifest.2da64f2b51232ced853f9d52c0ef9e13.js"
+  "/precache-manifest.3f67a006aaffef8d975f898ebe43b894.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IEEE-REACT/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
